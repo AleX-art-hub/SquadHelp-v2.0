29b2de3451649c6d402d76685ab905be40e39e3a
BodNS <bondarenokn@gmail.com>
2021-07-04 12:53:02 +0300

    token client updated

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -1,23 +1,52 @@
-const createHttpError = require('http-errors')
-const {User} = require('./../models')
+const createHttpError = require('http-errors');
+const { User } = require('./../models');
+const AuthService = require('./../services/authService');
 
-exports.signInUser = async (req, res, next) => {
-    try{
-        const {
-            body: {email, password}
-        } = req;
-        const userInstance = await User.findOne({
-            where: {email},
-        });
-        if(userInstance && (await userInstance.comparePassword(password))){
-            const data = await
-            return res.end();
-        }
-        next(createHttpError(401, 'Incorrect password or email'));
-    } catch(err){
-        next(err);
+exports.login = async (req, res, next) => {
+  try {
+    const {
+      body: { email, password },
+    } = req;
+    const userInstance = await User.findOne({
+      where: { email },
+    });
+    if (userInstance && 
+        (await userInstance.comparePassword(password))) {
+        const data = await AuthService.createSession(userInstance);
+      return res.send({data});
     }
+    next(createHttpError(403, 'Incorrect password or email'));
+  } catch (err) {
+    next(err);
+  }
 };
-
-exports.signUpUser = async (req, res, next) => {};
-exports.refreshAuth = async (req, res, next) => {};
+exports.signUp = async (req, res, next) => {
+  try {
+    const {body} = req;
+    const userInstance = await User.create(body);
+    if (userInstance) {
+      const data = await AuthService.createSession(userInstance);
+      return res.send({ data });
+    }
+    next(createHttpError(401));
+  } catch (err) {
+    next(err);
+  }
+};
+exports.refresh = async (req, res, next) => {
+  try {
+    const {
+      body: { refreshToken },
+    } = req;
+    const refreshTokenInstance = await RefreshToken.findOne({
+      where: { token: refreshToken },
+    });
+    if (refreshTokenInstance) {
+        const data = await AuthService.refreshSession(refreshTokenInstance);
+      return res.send({data});
+    }
+    next(createHttpError(401));
+  } catch (err) {
+    next(err);
+  }
+};
